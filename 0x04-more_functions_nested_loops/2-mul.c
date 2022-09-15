965c0d718dd08a89ce7dbf50f1d4bd5d8b3ed66d
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2022-09-15 13:09:37 +0100

    begin next level

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ *
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return:  a *  b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
