130d0c3ecc4102f16dcfa8563c2c6606304bcadc
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2022-10-10 21:17:40 +0100

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+typedef struct dog dog_t;
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
